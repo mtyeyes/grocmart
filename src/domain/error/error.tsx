a1daaf8fee5c6bd8b9b1c40ccfd7c28a5b91e9a3
mtyeyes <mymagicbox@list.ru>
2020-09-16 02:33:52 +0300

    Error page

--- src/domain/error/error.tsx
+++ src/domain/error/error.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './error.styl';
+import Logo from '../../components/logo/logo';
+import LinkAsButton from '../../components/link-as-button/link-as-button';
 
 const Error = () => {
   return (
     <section className="error__wrapper">
-      <h2>404</h2>
-      <p>Page not found</p>
+      <Logo className="logo--centered" />
+      <h1 className="error__title">404</h1>
+      <p className="error__description">Page not found</p>
+      <LinkAsButton className="error__link" to="/" subtype="rectangular-red">Go to home page</LinkAsButton>
     </section>
   );
 };
